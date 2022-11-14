57055c67525b2ea8d400c4495b9813caa87b8d01
milos-rad <wdev555@gmail.com>
2022-11-14 02:13:30 -0500

    -

--- ecommerce/extensions/urls.py
+++ ecommerce/extensions/urls.py
@@ -6,9 +6,6 @@ from django.conf.urls import include, url
 payment = apps.get_app_config('payment')
 application = apps.get_app_config('ecommerce')
 
-logging.info("[logging info]")
-logging.info(application.urls[0])
-
 urlpatterns = [
     url(r'^api/', include(('ecommerce.extensions.api.urls', 'api'))),
     url(r'^payment/', include(payment.urls[0])),
